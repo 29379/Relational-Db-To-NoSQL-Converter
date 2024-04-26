867911bb82c0448aca393b449840df33cf6adad1
Katarzyna-Hajduk <72875387+Ssilith@users.noreply.github.com>
2024-04-26 19:13:42 +0200

    convert postgre to mongo one to one

--- one_to_one.py
+++ one_to_one.py
@@ -1,54 +1,85 @@
-import pymongo
-import json
 import datetime
+import pymongo
+import psycopg2
+import json
+from bson import ObjectId
+from bson.decimal128 import Decimal128
+from decimal import Decimal
 
-# one-to-one
-def create_db():
+def fetch_data_from_postgres(cursor, table_name, columns):
+    # fetch data from PostgreSQL
+    sql = f"SELECT {', '.join(columns)} FROM {table_name}"
+    cursor.execute(sql)
+    return cursor.fetchall()
+
+def convert_to_compatible_types(value):
+    # convert data to MongoDB compatible type
+    if isinstance(value, datetime.date) and not isinstance(value, datetime.datetime):
+        return datetime.datetime(year=value.year, month=value.month, day=value.day)
+    elif isinstance(value, datetime.time):
+        return str(value)
+    elif isinstance(value, Decimal):
+        return Decimal128(value)
+    return value
+
+def create_db(cursor, db, schema, relationships):
+    # populate with data
+    for collection_name, columns_info in schema.items():
+        collection = db[collection_name]
+        column_names = [col["column_name"] for col in columns_info]
+        data_from_pg = fetch_data_from_postgres(cursor, collection_name, column_names)
+
+        for row in data_from_pg:
+            document = {col["column_name"]: convert_to_compatible_types(row[idx]) for idx, col in enumerate(columns_info)}
+            document = handle_relationships(collection_name, document, relationships, db)
+            collection.insert_one(document)
+
+def handle_relationships(collection_name, document, relationships, db):
+    # relationships between collections
+    for relation in relationships:
+        if relation['from'] == collection_name:
+            related_collection = relation['to']
+            related_id_keys = [related_collection + 'id', related_collection + '_id']  # Check both keys
+            for related_id_key in related_id_keys:
+                if related_id_key in document:
+                    # related document
+                    related_document = db[related_collection].find_one({'id': document[related_id_key]})
+                    if related_document:
+                        # if found replace id with ObjectId or Object
+
+                        # ObjectId
+                        # document[related_id_key] = ObjectId(related_document['_id'])
+
+                        # Object
+                        document[related_collection] = related_document
+                    else:
+                        # if not found, delete id field
+                        document.pop(related_id_key)
+                    break 
+    return document
+
+
+
+def main():
     client = pymongo.MongoClient("mongodb://localhost:27017/")
     db = client["zbd_czy_dojade"]
+    conn = psycopg2.connect(
+        dbname='zbd_czy_dojade',
+        user='postgres',
+        password='123456',
+        host='localhost',
+        port='5432'
+    )
+    cursor = conn.cursor()
 
     with open("schema_details.json", "r") as file:
         schema = json.load(file)
+        relationships = schema.pop("relationships", [])
 
-    for collection_name, columns in schema.items():
-        if collection_name != "relationships":
-            collection = db[collection_name]
-            indexes = []
+    create_db(cursor, db, schema, relationships)
 
-            document = {}
-
-            for column in columns:
-                column_name = column["column_name"]
-                data_type = column["data_type"]
-                if "constraint_type" in column:
-                    if column["constraint_type"] == "PRIMARY KEY":
-                        index_name = f"{collection_name}_{column_name}_index"
-                        indexes.append(([(column_name, pymongo.ASCENDING)], index_name))
-                    elif column["constraint_type"] == "FOREIGN KEY":
-                        # foreign keys 
-                        index_name = f"{collection_name}_{column_name}_fk_index"
-                        indexes.append(([(column_name, pymongo.ASCENDING)], index_name))
-                
-                # example data generation
-                if data_type == "character varying":
-                    document[column_name] = "example_text"[:column.get("max_length", len("example_text"))]
-                elif data_type == "bigint":
-                    document[column_name] = 123456789012345
-                elif data_type == "boolean":
-                    document[column_name] = True
-                elif data_type == "double precision":
-                    document[column_name] = 123.456
-                elif data_type == "timestamp without time zone":
-                    document[column_name] = datetime.datetime.now()
-
-            if indexes:
-                collection.create_indexes([pymongo.IndexModel(fields, name=name) 
-                for fields, name in indexes])
-            
-            collection.insert_one(document)
-
-def main():
-    create_db()
+    cursor.close()
+    conn.close()
 
 if __name__ == "__main__":
     main()
--- sql_to_json.py
+++ sql_to_json.py
@@ -1,52 +1,49 @@
 import psycopg2
 import json
 
-def get_schema_details(connection):
-    cursor = connection.cursor()
+def get_constraints(cursor):
+    primary_keys = {}
+    unique_keys = {}
+    foreign_keys = {}
 
-    # Fetch table details
     cursor.execute("""
-        SELECT c.table_name, c.column_name, c.data_type, c.character_maximum_length,
-            CASE WHEN EXISTS (
-                    SELECT 1
-                    FROM information_schema.table_constraints tc
-                    JOIN information_schema.key_column_usage kcu
-                        ON tc.constraint_name = kcu.constraint_name
-                    WHERE tc.constraint_type = 'PRIMARY KEY'
-                        AND kcu.table_name = c.table_name
-                        AND kcu.column_name = c.column_name
-            ) THEN 'PRIMARY KEY' END AS constraint_type
-        FROM information_schema.columns c
-        WHERE c.table_schema = 'public'
-        ORDER BY c.table_name, c.ordinal_position;
+        SELECT kcu.table_name, kcu.column_name
+        FROM information_schema.key_column_usage AS kcu
+        JOIN information_schema.table_constraints AS tc
+            ON kcu.table_schema = tc.table_schema
+                AND kcu.table_name = tc.table_name
+                AND kcu.constraint_name = tc.constraint_name
+        WHERE tc.constraint_type = 'PRIMARY KEY'
+            AND kcu.table_schema = 'public'
     """)
+    for table_name, column_name in cursor.fetchall():
+        if table_name not in primary_keys:
+            primary_keys[table_name] = []
+        primary_keys[table_name].append(column_name)
 
-    schema_details = {}
-    for row in cursor.fetchall():
-        table_name, column_name, data_type, character_maximum_length, constraint_type = row
-        if table_name not in schema_details:
-            schema_details[table_name] = []
+    cursor.execute("""
+        SELECT kcu.table_name, kcu.column_name
+        FROM information_schema.key_column_usage AS kcu
+        JOIN information_schema.table_constraints AS tc
+            ON kcu.table_schema = tc.table_schema
+                AND kcu.table_name = tc.table_name
+                AND kcu.constraint_name = tc.constraint_name
+        WHERE tc.constraint_type = 'UNIQUE'
+            AND kcu.table_schema = 'public'
+            AND kcu.column_name NOT IN (SELECT column_name FROM information_schema.key_column_usage WHERE table_schema = 'public' 
+                AND constraint_name IN (SELECT constraint_name FROM information_schema.table_constraints WHERE constraint_type = 'PRIMARY KEY'))
+    """)
+    for table_name, column_name in cursor.fetchall():
+        if table_name not in unique_keys:
+            unique_keys[table_name] = []
+        unique_keys[table_name].append(column_name)
 
-        column_details = {
-            'column_name': column_name,
-            'data_type': data_type
-        }
-
-        if data_type == 'character varying':
-            column_details['max_length'] = character_maximum_length
-            
-            column_details['constraint_type'] = constraint_type
-            
-        schema_details[table_name].append(column_details)
-
-     # Fetch foreign key constraints
     cursor.execute("""
         SELECT
             tc.table_name,
             kcu.column_name,
             ccu.table_name AS foreign_table_name,
-            ccu.column_name AS foreign_column_name,
-            fc.data_type AS foreign_column_data_type
+            ccu.column_name AS foreign_column_name
         FROM 
             information_schema.table_constraints AS tc 
             JOIN information_schema.key_column_usage AS kcu
@@ -55,34 +52,169 @@ def get_schema_details(connection):
             JOIN information_schema.constraint_column_usage AS ccu
                 ON ccu.constraint_name = tc.constraint_name
                     AND ccu.table_schema = tc.table_schema
-            JOIN information_schema.columns AS fc
-                ON ccu.column_name = fc.column_name
-                    AND ccu.table_name = fc.table_name
-                    AND ccu.table_schema = fc.table_schema
-        WHERE tc.constraint_type = 'FOREIGN KEY';
+        WHERE tc.constraint_type = 'FOREIGN KEY'
+            AND tc.table_schema = 'public';
+    """)
+    for row in cursor.fetchall():
+        table_name, column_name, foreign_table_name, foreign_column_name = row
+        if table_name not in foreign_keys:
+            foreign_keys[table_name] = []
+        foreign_keys[table_name].append({
+            'column_name': column_name,
+            'foreign_table_name': foreign_table_name,
+            'foreign_column_name': foreign_column_name
+        })
+
+    return primary_keys, unique_keys, foreign_keys
+
+
+def identify_junction_tables(foreign_keys, primary_keys, cursor):
+    junction_tables = {}
+    for table, fks in foreign_keys.items():
+        linked_tables = set(fk['foreign_table_name'] for fk in fks)
+
+        # ensure the table links to two or more distinct tables
+        if len(linked_tables) > 1:
+            cursor.execute("""
+                SELECT column_name
+                FROM information_schema.columns
+                WHERE table_name = %s AND table_schema = 'public'
+            """, (table,))
+            all_columns = set(row[0] for row in cursor.fetchall())
+            fk_columns = set(fk['column_name'] for fk in fks)
+            primary_key_columns = set(primary_keys.get(table, []))
+
+            # check if primary key columns include all foreign key columns
+            if fk_columns <= primary_key_columns:
+                junction_tables[table] = list(linked_tables)
+
+    return junction_tables
+
+
+def get_relationships(cursor, primary_keys, unique_keys, junction_tables):
+    # relationships
+    cursor.execute("""
+        SELECT
+            tc.table_name, 
+            kcu.column_name, 
+            ccu.table_name AS foreign_table_name, 
+            ccu.column_name AS foreign_column_name,
+            tc.constraint_name
+        FROM 
+            information_schema.table_constraints AS tc
+        JOIN 
+            information_schema.key_column_usage AS kcu 
+            ON tc.constraint_name = kcu.constraint_name
+        JOIN 
+            information_schema.constraint_column_usage AS ccu 
+            ON ccu.constraint_name = tc.constraint_name
+        WHERE 
+            tc.constraint_type = 'FOREIGN KEY' AND 
+            tc.table_schema = 'public'
     """)
 
-    for row in cursor.fetchall():
-        table_name, column_name, foreign_table_name, foreign_column_name, foreign_column_data_type = row
-        
-        for column in schema_details[table_name]:  # If column found, update not to duplicate
-            if column['column_name'] == column_name:
-                column.update({
-                    'constraint_type': 'FOREIGN KEY',
-                    'foreign_table': foreign_table_name,
-                    'foreign_column': foreign_column_name
-                })
-                break
-        else:  # If column not found, add new
-            schema_details[table_name].append({
-                'column_name': column_name,
-                'data_type': foreign_column_data_type,
-                'constraint_type': 'FOREIGN KEY',
-                'foreign_table': foreign_table_name,
-                'foreign_column': foreign_column_name
-            })
+    relationships = {}
+    for table_name, column_name, foreign_table_name, foreign_column_name, constraint_name in cursor.fetchall():
+        if column_name in unique_keys.get(table_name, []):
+            relationship_type = 'one-to-one'
+        elif table_name not in junction_tables:
+            relationship_type = 'many-to-one'
+        else:
+            relationship_type = 'many-to-many'
 
-    return schema_details
+        if table_name not in relationships:
+            relationships[table_name] = []
+
+        relationships[table_name].append({
+            'column': column_name,
+            'foreign_table': foreign_table_name,
+            'foreign_column': foreign_column_name,
+            'relationship_type': relationship_type
+        })
+
+    return relationships
+
+    
+def get_schema_details(connection):
+    cursor = connection.cursor()
+
+    primary_keys, unique_keys, foreign_keys = get_constraints(cursor)
+    junction_tables = identify_junction_tables(foreign_keys, primary_keys, cursor)
+    relationship_data = get_relationships(cursor, primary_keys, unique_keys, junction_tables)
+
+    # get details about each tables columns
+    cursor.execute("""
+        SELECT c.table_name, c.column_name, c.data_type, 
+               c.character_maximum_length,
+               array_agg(tc.constraint_type) FILTER (WHERE tc.constraint_type IN ('PRIMARY KEY', 'UNIQUE')) AS constraint_types,
+               c.ordinal_position
+        FROM information_schema.columns c
+        LEFT JOIN information_schema.key_column_usage AS kcu
+            ON c.column_name = kcu.column_name
+                AND c.table_name = kcu.table_name
+        LEFT JOIN information_schema.table_constraints AS tc
+            ON kcu.table_schema = tc.table_schema
+                AND kcu.table_name = tc.table_name
+                AND kcu.constraint_name = tc.constraint_name
+        WHERE c.table_schema = 'public'
+        GROUP BY c.table_name, c.column_name, c.data_type, c.character_maximum_length, c.ordinal_position
+        ORDER BY c.table_name, c.ordinal_position;
+    """)
+
+    schema_details = {}
+
+    # column details
+    for row in cursor.fetchall():
+        table_name, column_name, data_type, character_maximum_length, constraint_types, ordinal_position = row
+        if constraint_types is None:
+            constraint_types = []
+
+        if table_name not in schema_details:
+            schema_details[table_name] = []
+
+        column_details = {
+            'column_name': column_name,
+            'data_type': data_type
+        }
+
+        if data_type == 'character varying' and character_maximum_length is not None:
+            column_details['max_length'] = character_maximum_length
+
+        if 'PRIMARY KEY' in constraint_types:
+            column_details['constraint_type'] = 'PRIMARY KEY'
+        elif 'UNIQUE' in constraint_types:
+            column_details['constraint_type'] = 'UNIQUE'
+
+        schema_details[table_name].append(column_details)
+    
+    # foreign keys
+    for table_name in schema_details:
+        for column in schema_details[table_name]:
+            for fk in foreign_keys.get(table_name, []):
+                if column['column_name'] == fk['column_name']:
+                    column.update({
+                        'foreign_table': fk['foreign_table_name'],
+                        'foreign_column': fk['foreign_column_name'],
+                        'constraint_type': 'FOREIGN KEY'
+                    })
+
+    # adding relations
+    relationship_summary = []
+    for table, rels in relationship_data.items():
+        for rel in rels:
+            relationship_detail = {
+                'from': table,
+                'to': rel['foreign_table'],
+                'type': rel['relationship_type']
+            }
+            relationship_summary.append(relationship_detail)
+
+
+    schema_details['relationships'] = relationship_summary
+
+    cursor.close()
+    return dict(schema_details)
+
 
 def save_to_json(data, filename):
     with open(filename, 'w') as json_file:
@@ -92,7 +224,7 @@ def main():
     conn = psycopg2.connect(
         dbname='zbd_czy_dojade',
         user='postgres',
-        password='asdlkj000',
+        password='123456',
         host='localhost',
         port='5432'
     )
