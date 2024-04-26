ffadf732c330481297a79e876f143a44d9649c24
Kulej <jkul@thaumatec.com>
2024-04-26 09:23:33 +0200

    update

--- insert_data_into_db/src/main.rs
+++ insert_data_into_db/src/main.rs
@@ -1,7 +1,10 @@
+use std::collections::HashMap;
+
 fn main() {
     let trips = include_str!("../../source_data/OtwartyWroclaw_rozklad_jazdy_GTFS/trips.txt");
     
-
+    let mut headsigns: HashMap<&str,usize> = HashMap::new();
+    let mut latest_key = 0;
     let mut trips_iter = trips.split("\n");
 
     trips_iter.next();
@@ -13,6 +16,13 @@ fn main() {
             .unwrap();
 
             println!("{}",headsign);
+            headsigns
+                .entry(headsign)
+                .or_insert_with(|| {
+                    latest_key = latest_key + 1;
+                    latest_key
+                });
 
             });
+    print!("{:?}",headsigns)
 }
