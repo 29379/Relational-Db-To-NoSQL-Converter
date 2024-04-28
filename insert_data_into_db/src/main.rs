4b0d53e8fcda74cc1196be2499f531dbd3d59831
Kulej <jkul@thaumatec.com>
2024-04-28 15:11:13 +0200

    Moved reading routes.txt to module routes

--- insert_data_into_db/src/main.rs
+++ insert_data_into_db/src/main.rs
@@ -1,26 +1,7 @@
-use std::{collections::HashMap, path::Path, usize};
+use std::collections::HashMap;
+
+pub mod routes;
 
 fn main() {
-    let path = Path::new("../../source_data/OtwartyWroclaw_rozklad_jazdy_GTFS/trips.txt");
-    let mut reader = csv::Reader::from_path(path).unwrap();
-    let mut trip_heads: HashMap<String, usize> = HashMap::new();
-    let mut trip_heads_count = 0;
-
-    reader
-        .records()
-        .for_each(|record_result| match record_result {
-            Ok(record) => {
-                trip_heads
-                    .entry(record.get(3).unwrap().to_string())
-                    .or_insert_with(|| {
-                        let count: usize = {
-                            trip_heads_count = trip_heads_count + 1;
-                            trip_heads_count
-                        };
-                        count
-                    });
-            }
-            Err(e) => println!("{}", e),
-        });
-    print!("{:?}",trip_heads);
+    routes::print_collection();
 }
---
+++ insert_data_into_db/src/routes.rs
@@ -0,0 +1,30 @@
+pub struct Test{}
+
+pub fn print_collection(){
+
+use crate::HashMap;
+use std::path::Path;
+
+let path = Path::new("../../source_data/OtwartyWroclaw_rozklad_jazdy_GTFS/trips.txt");
+    let mut reader = csv::Reader::from_path(path).unwrap();
+    let mut trip_heads: HashMap<String, usize> = HashMap::new();
+    let mut trip_heads_count = 0;
+
+    reader
+        .records()
+        .for_each(|record_result| match record_result {
+            Ok(record) => {
+                trip_heads
+                    .entry(record.get(3).unwrap().to_string())
+                    .or_insert_with(|| {
+                        let count: usize = {
+                            trip_heads_count = trip_heads_count + 1;
+                            trip_heads_count
+                        };
+                        count
+                    });
+            }
+            Err(e) => println!("{}", e),
+        });
+    print!("Collection : {:?}",trip_heads);
+}
