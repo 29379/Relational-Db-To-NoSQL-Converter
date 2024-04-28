4aecbcee91a5bb208e8defaf78e90c1db91d2297
Kulej <jkul@thaumatec.com>
2024-04-28 13:22:16 +0200

    Correctly reading headings

--- insert_data_into_db/src/main.rs
+++ insert_data_into_db/src/main.rs
@@ -1,16 +1,18 @@
-use std::path::Path;
+use std::{collections::HashMap, path::Path, usize};
 
 
 fn main() {
     let path = Path::new("../../source_data/OtwartyWroclaw_rozklad_jazdy_GTFS/trips.txt");
-    let path_test = Path::new("../../source_data");
-
-    println!("{}",path_test.exists());
- //let trips = include_bytes!("../../source_data/OtwartyWroclaw_rozklad_jazdy_GTFS/trips.txt");
     let mut reader = csv::Reader::from_path(path).unwrap();
+    let mut trip_heads:HashMap<&str, usize> = HashMap::new();
 
-    reader.records().for_each(|record| {
-        println!("{:?}",record);
+    reader.records().for_each(|record_result| {
+        match record_result {
+            Ok(record) =>{
+                println!("{:?}",record.get(3).unwrap());
+            },
+            Err(e) => println!("{}",e),
+        }
     })
 
     
