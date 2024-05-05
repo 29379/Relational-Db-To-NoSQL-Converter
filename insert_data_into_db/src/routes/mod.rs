bd085c444b7b607646d3d6c6ba2684ce4fce38f9
jakulej <j.kulej01@gmail.com>
2024-05-05 19:25:38 +0200

    Update

--- insert_data_into_db/src/routes/mod.rs
+++ insert_data_into_db/src/routes/mod.rs
@@ -1,48 +1,69 @@
 use serde::Deserialize;
 
-use std::path::Path;
+use std::{collections::HashMap, path::Path};
 pub mod stop;
 
 #[derive(Deserialize)]
 pub struct Record {
     route_id: String,
+    #[allow(dead_code)]
     agency_id: usize,
+    #[allow(dead_code)]
     route_short_name: String,
+    #[allow(dead_code)]
     route_long_name: String,
     route_desc: String,
     route_type: usize,
+    #[allow(dead_code)]
     route_type2_id: usize,
+    #[allow(dead_code)]
     valid_from: String,
+    #[allow(dead_code)]
     valid_until: String,
 }
-
-pub fn get_collections() {
-    let stop_id = stop::get_stop_id();
+//                              route, route_Stop, stop
+pub fn get_collections() -> (Vec<(String, usize)>,Vec<(usize, String, usize, usize)>,Vec<(usize, usize, String, f64, f64)>){
+    let mut stops_id = stop::get_stop_id();
+    let mut last_stop_id = get_last_id(&stops_id);
     let mut route_stop: Vec<(usize, String, usize, usize)> = Vec::new();
     //(id, route_id, stop_id, current_stop_in_route)
-    let mut id_count = 0usize;
+    let mut id_route_stop = 0usize;
+
+    let mut route: Vec<(String, usize)> = Vec::new();
 
     let path = Path::new("../../source_data/OtwartyWroclaw_rozklad_jazdy_GTFS/routes.txt");
     csv::Reader::from_path(path)
         .unwrap()
         .deserialize::<Record>()
         .for_each(|record_result| match record_result {
-            Ok(record) => record
-                .route_desc
-                .split(" - ")
-                .enumerate()
-                .for_each(|(i, stop_name)| {
-                    id_count = id_count +1;
-                    let current_id = id_count;
-                    route_stop.push((current_id, record.route_id.clone(),match stop_id.get(stop_name){
-                        Some(stop_id) => *stop_id,
-                        None => {
-                            println!("Nie ma takiego klucza dla przystanku: {}",stop_name);
-                        0
-                        }
-                    },i))
-                }),
+            Ok(record) => {
+                route.push((record.route_id.clone(), record.route_type));
+                record
+                    .route_desc
+                    .split(" - ")
+                    .enumerate()
+                    .for_each(|(i, stop_name)| {
+                        id_route_stop = id_route_stop + 1;
+                        let current_id = id_route_stop;
+                        route_stop.push((
+                            current_id,
+                            record.route_id.clone(),
+                            stops_id
+                                .entry((*stop_name).to_string())
+                                .or_insert_with(|| {
+                                    last_stop_id = last_stop_id + 1;
+                                    last_stop_id
+                                })
+                                .clone(),
+                            i,
+                        ))
+                    })
+            }
             Err(e) => println!("{}", e),
         });
-    println!("{:?}",route_stop);
+    (route,route_stop,stop::create_stop(stops_id))
+}
+
+fn get_last_id(hash_map: &HashMap<String, usize>) -> usize {
+    hash_map.values().max().unwrap().clone()
 }
--- insert_data_into_db/src/routes/stop.rs
+++ insert_data_into_db/src/routes/stop.rs
@@ -14,9 +14,8 @@ pub struct Record {
     stop_lon: f64,
 }
 
-pub fn get_stop_id() -> HashMap<String,usize>{
-    
-    let mut stop_id:HashMap<String,usize> = HashMap::new();
+pub fn get_stop_id() -> HashMap<String, usize> {
+    let mut stop_id: HashMap<String, usize> = HashMap::new();
 
     let path = Path::new("../../source_data/OtwartyWroclaw_rozklad_jazdy_GTFS/stops.txt");
     csv::Reader::from_path(path)
@@ -25,8 +24,34 @@ pub fn get_stop_id() -> HashMap<String,usize>{
         .for_each(|record_result| match record_result {
             Ok(record) => {
                 stop_id.insert(record.stop_name, record.stop_id);
-            },
-            Err(e) => println!("{}",e),
+            }
+            Err(e) => println!("{}", e),
         });
     stop_id
+}
+
+pub fn create_stop(mut stops: HashMap<String, usize>) -> Vec<(usize, usize, String, f64, f64)> {
+    let mut stop: Vec<(usize, usize, String, f64, f64)> = Vec::new();
+
+    let path = Path::new("../../source_data/OtwartyWroclaw_rozklad_jazdy_GTFS/stops.txt");
+    csv::Reader::from_path(path)
+        .unwrap()
+        .deserialize::<Record>()
+        .for_each(|record_result| match record_result {
+            Ok(record) => stop.push((
+                match stops.remove_entry(&record.stop_name) {
+                    Some((_key, value)) => value,
+                    None => {
+                        println!("Nie ma takiego klucza");
+                        0
+                    }
+                },
+                record.stop_code,
+                record.stop_name,
+                record.stop_lat,
+                record.stop_lon
+            )),
+            Err(e) => println!("{}", e),
+        });
+    stop
 }
