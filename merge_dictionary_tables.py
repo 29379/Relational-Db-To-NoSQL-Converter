450384958d670ee133a844630af89bd3c14e869e
Jakub Krupiński <jakub.krupinski20.03@gmail.com>
2024-05-06 20:37:11 +0200

    refractor schema_details dla many-to-one prawie done

--- merge_dictionary_tables.py
+++ merge_dictionary_tables.py
@@ -6,14 +6,18 @@ from bson import ObjectId
 from bson.decimal128 import Decimal128
 from decimal import Decimal
 
+# TODO: delete relation from route_type to route_type /
+#   trip_destination to trip_destination in relationships in after.json
+# TODO: merging 1:1 relationships
+# TODO: merging the names of the tables
+# TODO: merging the postgresql data
 
 def create_db(cursor, db, schema, relationships):
+    handle_merging_tables_relationships(schema, relationships)
     # populate with data
     for collection_name, columns_info in schema.items():
         collection = db[collection_name]
         column_names = [col["column_name"] for col in columns_info]
-
-        handle_merging_tables_relationships(cursor, schema, relationships)
 
         data_from_pg = fetch_data_from_postgres(cursor, collection_name, column_names)
 
@@ -26,15 +30,15 @@ def create_db(cursor, db, schema, relationships):
             collection.insert_one(document)
 
 
-def handle_merging_tables_relationships(cursor, schema, relationships, merged_tables=set()):
+def handle_merging_tables_relationships(schema, relationships, merged_tables=set()):
     for relation in relationships:
         if relation["from"] in merged_tables or relation["to"] in merged_tables:
             continue
         for label, value in relation.items():
             if label == "type" and value == "many-to-one":
-                num = evaluate_if_dictionary_table(schema[relation["to"]])
+                is_a_dictionary_table = check_if_is_a_dictionary_table(schema[relation["to"]])
                 # print("From: " + relation["from"] + " To: " + relation["to"] + " Num: " + str(num))
-                if num <= 1:
+                if is_a_dictionary_table:
                     fix_schema_details(
                         schema,
                         relationships,
@@ -42,27 +46,29 @@ def handle_merging_tables_relationships(cursor, schema, relationships, merged_ta
                         relation["to"]
                     )
                     merged_tables.add(relation["to"])
+                    merged_tables.add(relation["from"])
                     #   recursive call - if a table 'relation["to"]' was merged into table 'relation["from"]',
                     #   then 'relation["to"]' table ceases to exist, so we can skip it, and the table
                     #   'relation["from"]' does not meet the criteria for further evaluation anymore,
                     #   so we can skip it as well
-                    handle_merging_tables_relationships(cursor, schema, relationships, merged_tables)
+                    handle_merging_tables_relationships(schema, relationships, merged_tables)
 
 
-def evaluate_if_dictionary_table(columns_info):
-    num_of_columns = len(columns_info)
+def check_if_is_a_dictionary_table(columns_info):
+    num_of_regular_columns = 0
+
     for cols in columns_info:
-        for label, info in cols.items():
-            if label == "constraints" and info is not None and info != []:
-                for constraint in info:
-                    if "KEY" in constraint:
-                        num_of_columns -= 1
-                        break
-    return num_of_columns
+        if cols["constraints"] is not None \
+                and cols["constraints"] != []:
+            if "PRIMARY KEY" not in cols["constraints"] \
+                    and "FOREIGN KEY" not in cols["constraints"]:
+                num_of_regular_columns += 1
+        else:
+            num_of_regular_columns += 1
+    return num_of_regular_columns == 1
 
 
 def fix_schema_details(schema, relationships, from_t, to_t):
-    print(schema)
     for relation in relationships:
         if relation["from"] == to_t:
             relation["from"] = from_t
@@ -71,17 +77,37 @@ def fix_schema_details(schema, relationships, from_t, to_t):
     if to_t in schema and from_t in schema:
         to_t_accumulator = schema.pop(to_t)
         primary_key_column = None
+        filtered_columns = []
 
         for column in to_t_accumulator:
-            if column["constraints"] is not None and column["constraints"] != []:
+            if "constraints" in column \
+                    and column["constraints"] != []:
                 if "PRIMARY KEY" in column["constraints"]:
                     primary_key_column = column
                     break
+  
+        for column in schema[from_t]:
+            if not ("FOREIGN KEY" in column.get("constraints") \
+                    and to_t == column.get("foreign_table")):
+                filtered_columns.append(column)
+
+        print(filtered_columns)
+        schema[from_t] = filtered_columns
         for column in to_t_accumulator:
             if column != primary_key_column:
                 schema[from_t].append(column)
-    print("\n- - - - - - - - - -\n")
-    print(schema)
+
+    with open('after.json', 'w') as after_file:
+        json.dump(
+            {"schema": schema, 
+             "relationships": relationships},
+             after_file, 
+             indent=4
+        )
+
+
+def check_if_is_a_deprecated_foreign_key(columns_info):
+    pass
 
 
 def fetch_data_from_postgres(cursor, table_name, columns):
