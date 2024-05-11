5443d65cb836d2f58cf43b5bae03f45e38912915
Katarzyna-Hajduk <72875387+Ssilith@users.noreply.github.com>
2024-05-11 15:25:42 +0200

    console app

--- create_erd.py
+++ create_erd.py
@@ -1,8 +1,5 @@
-import psycopg2
 from eralchemy import render_er
 
-database_uri = "postgresql+psycopg2://postgres:123456@localhost/zbd_czy_dojade"
 
-output_path = "diagram.png"
-
-render_er(database_uri, output_path)
+def create_erd(uri, path):
+    render_er(uri, path)
