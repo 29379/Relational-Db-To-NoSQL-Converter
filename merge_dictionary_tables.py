4b6fe98def1841e37171fb99ce5d4f8792b541dc
Jakub Krupiński <jakub.krupinski20.03@gmail.com>
2024-05-12 14:14:35 +0200

    self referencing tables will be a problem

--- merge_dictionary_tables.py
+++ merge_dictionary_tables.py
@@ -7,20 +7,34 @@ from bson.decimal128 import Decimal128
 from decimal import Decimal
 
 # TODO: merging 1:1 relationships
+# TODO: naming in the junction tables and schema
 # TODO: merging the postgresql data
+# TODO: handling self referencing tables - big trouble
 
 
-def is_id_column(column_name, pattern):
-    column_name = column_name.lower()
-    patterns = [
-        pattern + "id",
-        pattern + "_id",
-    ]
-    return column_name in patterns
+def is_id_column(column_name, pattern, patterns=None):
+    if patterns is None:
+        patterns = [pattern + "id", pattern + "_id"]
+    return column_name.lower() in patterns
 
 
 def create_meged_table_name(from_table, to_table):
     return from_table + "__" + to_table
+
+
+def check_if_is_a_dictionary_table(columns_info):
+    num_of_regular_columns = 0
+
+    for cols in columns_info:
+        if cols["constraints"] is not None \
+                and cols["constraints"] != []:
+            if "PRIMARY KEY" not in cols["constraints"] \
+                    and "FOREIGN KEY" not in cols["constraints"]:
+                num_of_regular_columns += 1
+        else:
+            num_of_regular_columns += 1
+    print("num_of_regular_columns: " + str(num_of_regular_columns))
+    return num_of_regular_columns == 1
 
 
 def create_db(cursor, db, schema, relationships):
@@ -42,55 +56,95 @@ def create_db(cursor, db, schema, relationships):
 
 
 def handle_merging_tables_relationships(schema, relationships, merged_tables=set()):
+    # Process one-to-one relationships first
     for relation in relationships:
         if relation["from"] in merged_tables or relation["to"] in merged_tables:
             continue
-        for label, value in relation.items():
-            if label == "type" and value == "many-to-one":
-                is_a_dictionary_table = check_if_is_a_dictionary_table(schema[relation["to"]])
-                if is_a_dictionary_table:
-                    fix_schema_details(
-                        schema,
-                        relationships,
-                        relation["from"],
-                        relation["to"]
-                    )
-                    merged_tables.add(relation["to"])
-                    merged_tables.add(relation["from"])
-                    merged_tables.add(create_meged_table_name(relation["from"], relation["to"]))
-                    #   recursive call - if a table 'relation["to"]' was merged into table 'relation["from"]',
-                    #   then 'relation["to"]' table ceases to exist, so we can skip it, and the table
-                    #   'relation["from"]' does not meet the criteria for further evaluation anymore,
-                    #   so we can skip it as well
-                    handle_merging_tables_relationships(schema, relationships, merged_tables)
-            elif label == "type" and value == "one-to-one":
-                # TODO: here
-                pass
-
-
-def check_if_is_a_dictionary_table(columns_info):
-    num_of_regular_columns = 0
-
-    for cols in columns_info:
-        if cols["constraints"] is not None \
-                and cols["constraints"] != []:
-            if "PRIMARY KEY" not in cols["constraints"] \
-                    and "FOREIGN KEY" not in cols["constraints"]:
-                num_of_regular_columns += 1
-        else:
-            num_of_regular_columns += 1
-    return num_of_regular_columns == 1
-
-
-def fix_schema_details(schema, relationships, from_t, to_t):
-    relations_tmp = []
-    merged_table_name = create_meged_table_name(from_t, to_t)
+        if relation.get("type") == "one-to-one":
+            merge_one_to_one(schema, relationships, relation, merged_tables)
+            
+    # Process many-to-one relationships afterwards
     for relation in relationships:
-        if relation["from"] == to_t:
-            relation["from"] = merged_table_name
-            relations_tmp.append(relation)
-        elif relation["to"] == to_t:
-            if relation["from"] == from_t:
+        if relation["from"] in merged_tables or relation["to"] in merged_tables:
+            continue
+        if relation.get("type") == "many-to-one":
+            merge_many_to_one(schema, relationships, relation, merged_tables)
+    # for relation in relationships:
+    #     if relation["from"] in merged_tables or relation["to"] in merged_tables:
+    #         # print(merged_tables)
+    #         continue
+    #     for label, value in relation.items():
+    #         if label == "type" and value == "many-to-one":
+    #             merge_many_to_one(schema, relationships, relation, merged_tables)
+    #         elif label == "type" and value == "one-to-one":
+    #             merge_one_to_one(schema, relationships, relation, merged_tables)
+
+
+def merge_many_to_one(schema, relationships, relation, merged_tables):
+    print("checking if is a dictionary table: " + relation["to"])
+    if relation["to"] == "trip_destination":
+        print("-----------------------------------------------")
+    is_a_dictionary_table = check_if_is_a_dictionary_table(schema[relation["to"]])
+
+    if is_a_dictionary_table:
+        print("\nmany-to-one merging (IF): - from: " + relation["from"] + " to: " + relation["to"] + "\n")
+        fix_schema_details(
+            schema,
+            relationships,
+            relation["from"],
+            relation["to"]
+        )
+        merged_tables.add(relation["to"])
+        merged_tables.add(relation["from"])
+        # merged_tables.add(create_meged_table_name(relation["from"], relation["to"]))
+        
+        #   recursive call - if a table 'relation["to"]' was merged into table 'relation["from"]',
+        #   then 'relation["to"]' table ceases to exist, so we can skip it, and the table
+        #   'relation["from"]' does not meet the criteria for further evaluation anymore,
+        #   so we can skip it as well
+        handle_merging_tables_relationships(schema, relationships, merged_tables)
+    else:
+        is_a_dictionary_table = check_if_is_a_dictionary_table(schema[relation["from"]])
+        if is_a_dictionary_table:
+            print("\nmany-to-one merging (ELSE): - from: " + relation["from"] + " to: " + relation["to"] + "\n")
+            fix_schema_details(
+                schema,
+                relationships,
+                relation["to"],
+                relation["from"]
+            )
+            merged_tables.add(relation["to"])
+            merged_tables.add(relation["from"])
+            # merged_tables.add(create_meged_table_name(relation["from"], relation["to"]))
+            handle_merging_tables_relationships(schema, relationships, merged_tables)
+
+
+
+
+def merge_one_to_one(schema, relationships, relation, merged_tables):
+    print("\none-to-one merging: - from: " + relation["from"] + " to: " + relation["to"] + "\n")
+    fix_schema_details(
+        schema,
+        relationships,
+        relation["from"],
+        relation["to"]
+    )
+    merged_tables.add(relation["to"])
+    merged_tables.add(relation["from"])
+    # merged_tables.add(create_meged_table_name(relation["from"], relation["to"]))
+    handle_merging_tables_relationships(schema, relationships, merged_tables)
+
+
+def fix_schema_details(schema, relationships, cardinality_one, cardinality_many):
+    relations_tmp = []
+    merged_table_name = create_meged_table_name(cardinality_one, cardinality_many)
+    for relation in relationships:
+        if relation["from"] == cardinality_many:
+            if relation["to"] != cardinality_one:   # self referencing tables left the chat but noone has to know
+                relation["from"] = merged_table_name
+                relations_tmp.append(relation)
+        elif relation["to"] == cardinality_many:
+            if relation["from"] == cardinality_one:
                 pass
             else:
                 relation["to"] = merged_table_name
@@ -100,8 +154,11 @@ def fix_schema_details(schema, relationships, from_t, to_t):
     
     relationships[:] = relations_tmp
 
-    if to_t in schema and from_t in schema:
-        to_t_accumulator = schema.pop(to_t)
+    #   cardinality_one    -    route      |  trip_destination
+    #   cardinality_many   -    route_type |  trip
+
+    if cardinality_many in schema and cardinality_one in schema:
+        to_t_accumulator = schema.pop(cardinality_many)
         primary_key_column = None
         filtered_columns = []
 
@@ -112,20 +169,20 @@ def fix_schema_details(schema, relationships, from_t, to_t):
                     primary_key_column = column
                     break
   
-        for column in schema[from_t]:
+        for column in schema[cardinality_one]:
             if not ("FOREIGN KEY" in column.get("constraints") \
-                    and to_t == column.get("foreign_table")):
+                    and cardinality_many == column.get("foreign_table")):
                 filtered_columns.append(column)
 
-        schema[from_t] = filtered_columns
+        schema[cardinality_one] = filtered_columns
         for column in to_t_accumulator:
             if column != primary_key_column:
-                schema[from_t].append(column)
-        schema[merged_table_name] = schema.pop(from_t)
+                schema[cardinality_one].append(column)
+        schema[merged_table_name] = schema.pop(cardinality_one)
         # print("renaming: " + from_t + " to " + merged_table_name)
-    print("\nfrom: " + from_t + " to: " + to_t + " into new name: " + merged_table_name + "\n")
-    rename_merged_table(schema, from_t, to_t, merged_table_name)
-    rename_merged_table(relationships, from_t, to_t, merged_table_name)
+    print("\nfrom: " + cardinality_one + " to: " + cardinality_many + " into new name: " + merged_table_name + "\n")
+    rename_merged_table(schema, cardinality_one, cardinality_many, merged_table_name)
+    rename_merged_table(relationships, cardinality_one, cardinality_many, merged_table_name)
 
     with open('after.json', 'w') as after_file:
         json.dump(
