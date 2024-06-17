3c9cd99797b7f311fba745006aac13f387fabbec
Katarzyna-Hajduk <72875387+Ssilith@users.noreply.github.com>
2024-06-17 23:45:14 +0200

    fix when user did not choose method properly

--- frontend/lib/convert/convert.dart
+++ frontend/lib/convert/convert.dart
@@ -188,6 +188,7 @@ class _ConvertState extends State<Convert> {
                     if (checkIfConfigurationIsNotValid()) {
                       message(context, 'Failure',
                           'Choose at least one relation type.');
+                      isLoading = false;
                       return;
                     }
 
