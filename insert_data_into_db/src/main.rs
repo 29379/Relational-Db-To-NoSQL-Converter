91d95403acd07f8802e84e866c31d91ece1c4894
jakulej <j.kulej01@gmail.com>
2024-06-21 10:44:56 +0200

    revRevert "correction"
    This reverts commit c8d5754e119110c946815c382639343108252cb9.

--- insert_data_into_db/src/main.rs
+++ insert_data_into_db/src/main.rs
@@ -6,7 +6,7 @@ pub mod trips;
 
 fn main() {
     let (trip, trip_headsign) = trips::get_collections();
-    let route = routes::get_route();
+    let (route,x,y) = routes::get_collections();
 
     //print Trip
     println!("id,direction_id,route_id,vechicle_id,trip_destination");
@@ -27,13 +27,6 @@ fn main() {
     route_stop.iter().for_each(|(key,value)|{
      println!("{},{},{},{}",value.0,key.0, key.1, value.1);
     });
-    
-    // Print schedule_stop_time
-    println!("id,route_id,stop_id,current_stop_in_route");
- //   let (route_stop, schedule_stop_time) = route_stop::get_route_stop(trips::trip_vec_to_hash_map(trip.clone()));
-   // trip.iter().for_each(|entry| {
-     //   println!("{},{},{},{}",entry.0, entry.1,entry.2,entry.3);
- //   });
 
     // Print route
     println!("id, route_type");
